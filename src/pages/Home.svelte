fe0d5e91ac3a90dda06c334ee2162423a977aaae
Roy Kuijper <roykuijper199716@hotmail.com>
2019-10-30 13:58:32 +0100

    Testing if the auto deployment works

--- src/pages/Home.svelte
+++ src/pages/Home.svelte
@@ -2,6 +2,6 @@
 import Nav from '../components/nav.svelte';
 </script>
 
- <h1>Cultural insights in the Edo period of Japan!</h1>
+ <h1>Cultural insights in the Edo period of Japan! test</h1>
 
 <Nav />
